dea5a8ff972c3225a202457c0eee2ec2bb0f99f8
fahadi04 <fahadmasrur786@gmail.com>
2024-08-17 10:49:33 +0530

    update in .json and vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base :"/Pass-Gen/",plugins: [react()],
+  base: "/Pass-gen-using-React-Gen/", plugins: [react()],
 })
